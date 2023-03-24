b4ecaf0e670aa35282313189d2beff13af7c4650
Sümeyye Barut <sbarut1@ford.com.tr>
2023-03-24 12:42:02 +0300

    Added healing for player

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE=10;
-const MONSTER_ATTACK_VALUE=15;
-const STRONG_ATTACK_VALUE=17;
+const MONSTER_ATTACK_VALUE=17;
+const STRONG_ATTACK_VALUE=14;
+const HEAL_VALUE=20;
 
 let chosenMaxLife=100;
 let currentMonsterHealht=chosenMaxLife;
@@ -8,16 +9,7 @@ let currentPlayerHealht=chosenMaxLife;
 
 
 adjustHealthBars(chosenMaxLife);
-
-function monsterAttack(mode){
-    let maxDamage;
-    if(mode==='ATTACK'){
-        maxDamage=ATTACK_VALUE;
-    }else if(mode==='STRONG_ATTACK'){
-        maxDamage=STRONG_ATTACK_VALUE;
-    }
-    const damage=dealMonsterDamage(maxDamage);
-    currentMonsterHealht-=damage;
+function EndRound(){
     const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealht-=playerDamage;
     if(currentMonsterHealht<=0&& currentPlayerHealht>0){
@@ -28,6 +20,17 @@ function monsterAttack(mode){
         alert('You have a draw')
     };
 }
+function monsterAttack(mode){
+    let maxDamage;
+    if(mode==='ATTACK'){
+        maxDamage=ATTACK_VALUE;
+    }else if(mode==='STRONG_ATTACK'){
+        maxDamage=STRONG_ATTACK_VALUE;
+    }
+    const damage=dealMonsterDamage(maxDamage);
+    currentMonsterHealht-=damage;
+    EndRound();
+}
 
 function attackHandler(){
    monsterAttack('ATTACK');
@@ -36,5 +39,18 @@ function strongAttackHandler(){
     monsterAttack('STRONG_ATTACK');
 
 }
+function healPlayerHandler(){
+    let healValue;
+    if(currentPlayerHealht>=chosenMaxLife-HEAL_VALUE){
+        alert('Your life is max');
+        healValue=chosenMaxLife-currentPlayerHealht;
+    }else {
+        healValue=HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealht+=healValue;
+    EndRound();
+}
 attackBtn.addEventListener('click',attackHandler);
 strongAttackBtn.addEventListener('click',strongAttackHandler);
+healBtn.addEventListener('click',healPlayerHandler);
