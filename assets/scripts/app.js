e3fa443cf9f15a0469d7eb4f853ac25045ad775a
Sümeyye Barut <sbarut1@ford.com.tr>
2023-03-26 23:53:20 +0300

    Added reset game function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,15 +3,40 @@ const MONSTER_ATTACK_VALUE=17;
 const STRONG_ATTACK_VALUE=14;
 const HEAL_VALUE=20;
 
-let chosenMaxLife=100;
+const MODE_ATTACK='ATTACK';//0
+const MODE_STRONG_ATTACK='STRONG_ATTACK';//1
+
+
+const enteredValue=prompt('Maximum life for you and monster.','100');
+
+
+let chosenMaxLife=parseInt(enteredValue);
+if(isNaN(chosenMaxLife)||chosenMaxLife<=0){
+    chosenMaxLife=100;
+}
+
 let currentMonsterHealht=chosenMaxLife;
 let currentPlayerHealht=chosenMaxLife;
-
+let hasBonusLife=true;
 
 adjustHealthBars(chosenMaxLife);
+
+function reset(){
+     currentMonsterHealht=chosenMaxLife;
+     currentPlayerHealht=chosenMaxLife;
+     resetGame(chosenMaxLife);
+}
+
 function EndRound(){
+    const initialPlayerHealt=currentPlayerHealht;
     const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealht-=playerDamage;
+    if(currentMonsterHealht<=0&&hasBonusLife){
+        hasBonusLife=false;
+        removeBonusLife();
+        currentPlayerHealht=initialPlayerHealt;
+    }
+
     if(currentMonsterHealht<=0&& currentPlayerHealht>0){
         alert('You won');
     }else if (currentPlayerHealht<=0&&currentMonsterHealht>0) {
@@ -19,12 +44,15 @@ function EndRound(){
     }else if (currentMonsterHealht<=0&& currentPlayerHealht<=0){
         alert('You have a draw')
     };
+    if(currentMonsterHealht<=0||currentPlayerHealht<=0){
+        reset();
+    }
 }
 function monsterAttack(mode){
     let maxDamage;
-    if(mode==='ATTACK'){
+    if(mode===MODE_ATTACK){
         maxDamage=ATTACK_VALUE;
-    }else if(mode==='STRONG_ATTACK'){
+    }else if(mode===MODE_STRONG_ATTACK){
         maxDamage=STRONG_ATTACK_VALUE;
     }
     const damage=dealMonsterDamage(maxDamage);
@@ -33,10 +61,10 @@ function monsterAttack(mode){
 }
 
 function attackHandler(){
-   monsterAttack('ATTACK');
+   monsterAttack(MODE_ATTACK);
 };
 function strongAttackHandler(){
-    monsterAttack('STRONG_ATTACK');
+    monsterAttack(MODE_STRONG_ATTACK);
 
 }
 function healPlayerHandler(){
